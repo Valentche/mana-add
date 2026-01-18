ac5c0094990e2780252b2728104560596bb245c8
Valentche <pablo.valente@uniplaclages.edu.br>
2026-01-17 23:01:35 -0300

    eu to ficando louco

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,16 +1,4 @@
-import React from 'react';
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-
-export default function ModelUser() {
-  return (
-    <div className="p-6">
-      <Card>
-        <CardHeader>
-          <CardTitle>backend/src/models/User.js</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <pre className="bg-slate-900 text-slate-100 p-4 rounded-lg overflow-x-auto text-xs">
-{`const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -31,10 +19,4 @@ const userSchema = new mongoose.Schema({
   }
 }, { timestamps: true });
 
-module.exports = mongoose.model('User', userSchema);`}
-          </pre>
-        </CardContent>
-      </Card>
-    </div>
-  );
-}
+module.exports = mongoose.model('User', userSchema);
